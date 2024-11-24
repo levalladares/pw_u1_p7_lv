4cf873422f35e5fc1d17a183c4dcd7c060285f85
Luis <levalladares>
2024-11-24 02:18:31 +0000

    Taller11

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,46 +1,82 @@
-var numero1 = 0;
-var numero2 = 0;
+var num1 = '';
+var num2 = '';
 var operacion = '';
+var ingresandoNum2 = false;
 
-function setarValor(valor){
-    let valorActual = document.getElementById('id_pantalla').innerText = valor;
-    console.log(valor);
-    document.getElementById('id_pantalla').innerText = valor;
-}
-//funcion para concatenar valores no sumar solo concatenar
-function setarValorConcatenado(valor){
-    let valorActual = document.getElementById('id_pantalla').innerText;
-    console.log("valor actual: "+valorActual);
-    document.getElementById('id_pantalla').innerText = valorActual + valor;
-}
-//funcion para limpiar pantalla
-function limpiarPantalla(valor){
-    if(valor === 'C'){
-        document.getElementById('id_pantalla').innerText = '0';
+//funcion setvalor: para mostrar en pantalla el valor de los botones
+function setarValor(valor) {
+    if (ingresandoNum2) {
+        num2 += valor; // Concatenar el valor a num2
+        document.getElementById('id_pantalla').innerText = num2;
+        console.log("num2: " + num2);
+    } else {
+        num1 += valor; // Concatenar el valor a num1
+        document.getElementById('id_pantalla').innerText = num1;
+        console.log("num1: " + num1);
     }
 }
 
-//funcion para recibir valor de los botones Y MOSTRAR EN PANTALLA
-function recibirValor(valor){
-    let valorActual = document.getElementById('id_pantalla').innerText;
-    if(valorActual === '0'){
-        setarValor(valor);
-    }else{
-        setarValorConcatenado(valor);
-        numero1=valorActual;
-        console.log("numero1: "+numero1);
+//funcion para seleccionar la operacion
+function seleccionarOperacion(op) {
+    operacion = op;
+    ingresandoNum2 = true;
+    switch (op) {
+        case '+':
+            document.getElementById('id_pantalla').innerText = '+';
+            break;
+        case '-':
+            document.getElementById('id_pantalla').innerText = '-';
+            break;
+        case 'X':
+            document.getElementById('id_pantalla').innerText = 'x';
+            break;
+        case '%':
+            document.getElementById('id_pantalla').innerText = '/';
+            break;
+        default:
+            resultado = 'Error';
+    }
+    console.log("operacion: " + operacion);
+}
+
+//funcion para realizar la operacion
+function calcular() {
+    let resultado;
+    switch (operacion) {
+        case '+':
+            resultado = parseFloat(num1) + parseFloat(num2);
+            break;
+        case '-':
+            resultado = parseFloat(num1) - parseFloat(num2);
+            break;
+        case 'X':
+            resultado = parseFloat(num1) * parseFloat(num2);
+            break;
+        case '%':
+            resultado = parseFloat(num1) / parseFloat(num2);
+            break;
+        default:
+            resultado = 'Error';
+    }
+    document.getElementById('id_pantalla').innerText = resultado;
+    console.log("resultado: " + resultado);
+}
+function limpiar() {
+    // Resetear valores
+    num1 = '';
+    num2 = '';
+    operacion = '';
+    ingresandoNum2 = false;
+    document.getElementById('id_pantalla').innerText = '';
+}
+function borrar() {
+    // Borrar el ultimo caracter
+    if (ingresandoNum2) {
+        num2 = num2.slice()
+        document.getElementById('id_pantalla').innerText = num2;
+    } else {
+        num1 = num1.slice(0, -1);
+        document.getElementById('id_pantalla').innerText = num1;
     }
 }
 
-//funcion para sumar
-function sumar(){
-    let valorActual = document.getElementById('id_pantalla').innerText;
-    numero1 = valorActual;
-    console.log("numero1: "+numero1);
-    document.getElementById('id_pantalla').innerText = '0';
-}
-//recibir operacion
-function recibirOperacion(valor){
-    operacion = valor;
-    console.log("operacion: "+operacion);
-}
